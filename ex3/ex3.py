38858eb33aaccc26bcc69247150cbb11d619aa8f
Glen Holcomb <DamnBigMan@gmail.com>
2012-05-23 10:36:51 -0600

    Added Comments to ex3 and added README.md

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,23 +1,38 @@
+# This line prints a message
 print "I will now count my chickens:"
 
+# Print Hens and the result of (30/6) + 5
 print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+# Print Roosters and the result of ((25 * 3) % 4) subtracted from 100 
+print "Roosters", 100 - 25 * 3 % 4 
 
+# More vanilla printing
 print "Now I will count the eggs:"
 
+# For some stupid reason we are having a very hard time calculating the number of eggs we have.
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# More vanilla printing
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Printing the result of 5 < -2
 print 3 + 2 < 5 - 7
 
+# Printing a string and the results of 3 + 2
 print "What is 3 + 2?", 3 + 2
+
+# Printing a string and the results of 5 - 7
 print "What is 5 - 7?", 5 - 7
 
+# Printing an earth shattering revelation.
 print "Oh, that's why it's False."
 
+# Hooray another vanilla string
 print "How about some more."
 
+# Printing a string and a boolean result
 print "Is it greater?", 5 > -2
+# Printing another string and a boolean result
 print "Is it greater or equal?", 5 >= -2
+# Printing another string and a boolean result
 print "Is it less or equal?", 5 <= -2
