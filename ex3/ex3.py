e614bf109960237145c1f38da82fc5b76b525d4f
Glen Holcomb <DamnBigMan@gmail.com>
2012-05-23 10:40:46 -0600

    Now counting fractional eggs.

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,28 +1,28 @@
 # This line prints a message
 print "I will now count my chickens:"
 
-# Print Hens and the result of (30/6) + 5
-print "Hens", 25 + 30 / 6
-# Print Roosters and the result of ((25 * 3) % 4) subtracted from 100 
-print "Roosters", 100 - 25 * 3 % 4 
+# Print Hens and the result of (30.0/6.0) + 5.0
+print "Hens", 25.0 + 30.0 / 6.0
+# Print Roosters and the result of ((25.0 * 3.0) % 4.0) subtracted from 100.0 
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 
 
 # More vanilla printing
 print "Now I will count the eggs:"
 
 # For some stupid reason we are having a very hard time calculating the number of eggs we have.
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # More vanilla printing
-print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
-# Printing the result of 5 < -2
-print 3 + 2 < 5 - 7
+# Printing the result of 5.0 < -2.0
+print 3.0 + 2.0 < 5.0 - 7.0
 
-# Printing a string and the results of 3 + 2
-print "What is 3 + 2?", 3 + 2
+# Printing a string and the results of 3.0 + 2.0
+print "What is 3.0 + 2.0?", 3.0 + 2.0
 
-# Printing a string and the results of 5 - 7
-print "What is 5 - 7?", 5 - 7
+# Printing a string and the results of 5.0 - 7.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
 
 # Printing an earth shattering revelation.
 print "Oh, that's why it's False."
@@ -31,8 +31,8 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Printing a string and a boolean result
-print "Is it greater?", 5 > -2
+print "Is it greater?", 5.0 > -2.0
 # Printing another string and a boolean result
-print "Is it greater or equal?", 5 >= -2
+print "Is it greater or equal?", 5.0 >= -2.0
 # Printing another string and a boolean result
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.0 <= -2.0
