7d18bc192d467078e005b18fab33571935895c28
Glen Holcomb <DamnBigMan@gmail.com>
2012-05-31 10:37:53 -0600

    New Form

---
+++ ex11/ex11.3.py
@@ -0,0 +1,6 @@
+color = raw_input('What is your color? ')
+home = raw_input('Where do you live? ')
+job = raw_input('What do you do? ')
+name = raw_input('What is your name? ')
+
+print "Hello %s, the %s from %s who really likes %s" % (name, job, home, color)
