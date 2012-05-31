847454c1fa29d9e0df71707ac43310cc1ccac4dd
Glen Holcomb <DamnBigMan@gmail.com>
2012-05-31 10:27:15 -0600

    Added a raw_input() statement with a prompt.

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -10,3 +10,5 @@ print "So, you're %r old %r tall and %r heavy." % (age, height, weight)
 # raw_input takes an optional string argument which will be displayed as a prompt
 #   it then reads from STDIN until it sees a new line.  The stream is converted
 #   to a string.  The trailing newline is stripped from the input.
+
+print raw_input('Echo> ')
