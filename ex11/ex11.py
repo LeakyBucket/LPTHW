a1be986edd3d770c15e49a8961d571e52e2efaac
Glen Holcomb <DamnBigMan@gmail.com>
2012-05-31 10:25:40 -0600

    Added raw_input explainantion.

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -6,3 +6,7 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you're %r old %r tall and %r heavy." % (age, height, weight)
+
+# raw_input takes an optional string argument which will be displayed as a prompt
+#   it then reads from STDIN until it sees a new line.  The stream is converted
+#   to a string.  The trailing newline is stripped from the input.
