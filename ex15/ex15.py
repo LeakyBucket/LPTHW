05a10405c39cd2f3c7174611b017b9453398649f
Glen Holcomb <DamnBigMan@gmail.com>
2012-06-12 09:00:24 -0600

    Removed the raw_input and second print section

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -11,14 +11,3 @@ txt = open(filename)
 print "Here's your file %r:" % filename
 # Printing the contents of the file assigned to txt
 print txt.read()
-
-# Printing a line of instruction
-print "Type the filename again:"
-# Displaying a prompt and assigning the input to file_again
-file_again = raw_input("> ")
-
-# Opening the file provided by the user and stored in file_again.  Assigning that to txt_again.
-txt_again = open(file_again)
-
-# Printing the contents of txt_again
-print txt_again.read()
