0d7aaadfd2321359c49e657c6fafef0dc306b3fa
Glen Holcomb <DamnBigMan@gmail.com>
2012-06-13 13:40:45 -0600

    Added formula in comment

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -28,6 +28,7 @@ print "Age: %d, Height: %d, Weight: %d ID: %d" % (age, height, weight, iq)
 # A puzzle for the extra credit, type it in anyway.
 print "Here is a puzzle."
 
+# 35 + (74 - 180 * (50/2))
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
