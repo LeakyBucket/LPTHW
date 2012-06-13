a68c12c0a5cb888ef66bd2fb17f9347bd51d6967
Glen Holcomb <DamnBigMan@gmail.com>
2012-06-13 13:10:28 -0600

    Wrote another function and called it 10 times.

--- ex19/ex19.py
+++ ex19/ex19.py
@@ -10,6 +10,10 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
   print "Get a blanket.\n"
 
 
+def echo(repeat):
+  print repeat
+
+
 # print a static message
 print "We can just give the function numbers directly:"
 # call cheese_and_crackers setting cheese_count to 20 and boxes_of_crackers to 30
@@ -34,3 +38,6 @@ cheese_and_crackers(10 + 20, 5 + 6)
 print "And we can combine the two, variables and math:"
 # call cheese_and_crackers with 110 for cheese_count and 1050 for boxes_of_crackers
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+for x in range(10):
+  echo(raw_input('... '))
