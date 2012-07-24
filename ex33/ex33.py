348d6d18bdf433c9a00adbfb6c731943c8f7c204
Glen Holcomb <DamnBigMan@gmail.com>
2012-07-24 10:23:55 -0600

    Using range now

--- ex33/ex33.py
+++ ex33/ex33.py
@@ -1,15 +1,9 @@
 numbers = []
 
 def count_and_fill(start, limit):
-  while start < limit:
-    print "At the top start is %d" % start
-    numbers.append(start)
+  return(range(start, limit))
 
-    start = start + 3
-    print "Numbers now: ", numbers
-    print "At the bottom start is %d" % start
-
-count_and_fill(0, 6)
+numbers = count_and_fill(0, 6)
 
 print "The numbers: "
 
